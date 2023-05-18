95567bee53c4c6df2e7ca9a677ce703d4527ed48
Tatyana Pyanova <tatyana.piyanova@gmail.com>
2023-05-18 19:48:17 +0300

    Расширен первый ФТ в комментариях, теперь использу ет unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,48 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import unittest
 
-options = Options()
-options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-browser = webdriver.Firefox(options=options)
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	'''Тест нового посетителя'''
+	
+	def setUp(self):
+		'''установка'''
+		options = Options()
+		options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+		self.browser = webdriver.Firefox(options=options)
 
-assert 'Django' in browser.title
+	
+	def tearDown(self):
+		'''демонтаж'''
+		self.browser.quit()
+
+	def	test_can_start_a_list_and_retrieve_it_later(self):
+		'''тест: можно начать список и получить его позже'''
+		# Эдит слышала про крутон новое онлайн-приложение со списком неотложных дел
+		# Она решает оценить его домашнюю страницу
+		self.browser.get('http://localhost:8000')
+
+		# Она видит что заголовок и шапка страницы говорят о списказ неотложных дел
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Закончить тест!')
+
+		# Ей сразу же предлагается ввести элемент списка
+
+		# Она набирает в текстовом поле "Купить павлиньи перья" (её хобби - вязание рыболовных мушек)
+
+		# Когда она нажимает enter, страница обновляется, и теперь страница содержит "1: Купить павлиньи перья" в качестве элемента списка
+
+		# Текстовое поле по-прежнему приглашает её добавить ещё один элемент
+		# Она вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична)
+
+		# Страница снова обновляется и теперь показывет оба элемента её списка
+
+		#Эдит интересно, запомнит ли сайт её список. Далее она видит, что сайт сгенерировал для неё уникальный URL-адрес - об этом выводится 		небольшой текст с объяснениями
+
+		# Она посещает этот URL-адрес - её списое по-прежнему там
+
+		# Удовлетворенная, она снова ложится спать
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
+ 
