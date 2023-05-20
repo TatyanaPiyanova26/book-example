71f3f2ddb8b80da8c942436a94f33765d86f64d2
Tatyana Pyanova <tatyana.piyanova@gmail.com>
2023-05-20 15:02:37 +0300

    Базовое представление теперь возвращает минимальный HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,21 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
 
-class SmokeTest(TestCase):
-    '''Тест на токсичность'''
+from lists.views import home_page
 
-    def test_bad_maths(self):
-        '''Тест: неправильные математические расчеты'''
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        '''Тест: корневой url преобразуется в представление домашней страницы'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        '''Тест: домашняя страницы возвращает правильный html'''
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
