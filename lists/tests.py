11c838c0a5ba1ffcee13ce3730a1bbe834bf7d53
Tatyana Pyanova <tatyana.piyanova@gmail.com>
2023-05-21 18:48:49 +0300

    Модель для элементов списка и связанная с ней миграция

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 
 from lists.views import home_page
 
+from lists.models import Item
 
 #from views import home_page
 
@@ -35,3 +36,23 @@ class HomePageTest(TestCase):
         self.assertTrue(html.endswith('</html>'))
 
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+    '''Тесст модели элемента списка'''
+    def test_saving_and_retrieving_items(self):
+        '''Тест сохранения и полечения элементов списка'''
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
